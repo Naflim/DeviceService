098b596c9f1f52c357eb1a767497eca70c6587a1
Naflim <naflimdante@gmail.com>
2022-02-15 11:16:32 +0800

    UHF09

---
+++ DeviceModel/UHFReader09.cs
@@ -0,0 +1,110 @@
+﻿using DeviceService.Model;
+using DeviceService.Model.ExceptionModels;
+using ReaderB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NaflimHelperLibrary;
+
+namespace DeviceService.DeviceModel
+{
+    public class UHFReader09 : IDevice
+    {
+        protected int port;                       //端口
+        protected byte comAdr = 0xFF;             //读写器地址
+        protected byte baud = 4;                  //波特率
+        protected int handle;                  //读写器链接句柄
+
+        byte adrTID = 02;
+        byte lenTID = 04;
+        byte tidFlag = 0;
+        byte[] epcList = new byte[5000];
+        int Totallen = 0;
+        int cardNum = 0;
+
+        /// <summary>
+        /// 自动串口连接
+        /// </summary>
+        public void Connect()
+        {
+            int linkflag = StaticClassReaderB.AutoOpenComPort(ref port, ref comAdr, baud, ref handle);
+
+            if (linkflag != 0) throw UHF09Exception.AbnormalJudgment(linkflag);
+        }
+        public void Connect(ConnectModel connect)
+        {
+            int linkflag = StaticClassReaderB.OpenComPort(connect.Port, ref comAdr, baud, ref handle);
+
+            if (linkflag != 0) throw UHF09Exception.AbnormalJudgment(linkflag);
+
+            port = connect.Port;
+        }
+
+        public void Disconnect()
+        {
+            int linkflag = StaticClassReaderB.CloseComPort();
+
+            if (linkflag != 0) throw UHF09Exception.AbnormalJudgment(linkflag);
+        }
+
+        /// <summary>
+        /// 设置功率
+        /// </summary>
+        /// <param name="power">功率值</param>
+        public void SetPower(byte power)
+        {
+            int powerflag = StaticClassReaderB.SetPowerDbm(ref comAdr, power, handle);
+
+            if (powerflag != 0) throw UHF288Exception.AbnormalJudgment(powerflag);
+        }
+
+        /// <summary>
+        /// 查询标签
+        /// </summary>
+        /// <returns>标签组</returns>
+        public virtual string[] SelTag()
+        {
+            int state = StaticClassReaderB.Inventory_G2(ref comAdr, adrTID, lenTID, tidFlag, epcList, ref Totallen, ref cardNum, handle);
+
+            if (state == 0x01 || state == 0x02 || state == 0x03 || state == 0x04)
+            {
+
+                int index = 0;
+
+                string[] epcArr = new string[cardNum];
+
+                for (int i = 0; i < cardNum; i++)
+                {
+                    int len = epcList[index];
+
+                    StringBuilder sb = new StringBuilder();
+
+                    for (int j = 0; j < len; j++)
+                        sb.Append(Convert.ToString(epcList[index + 1 + j], 16).PadLeft(2, '0').ToUpper());
+
+                    epcArr[i] = sb.ToString();
+
+                    index += len + 1;
+                }
+
+                return epcArr;
+            }
+            else return null;
+        }
+
+        /// <summary>
+        /// epc写入标签
+        /// </summary>
+        /// <param name="epc">写入的epc</param>
+        public void WriteTag(string epc)
+        {
+            int errorcode = 0;
+            byte[] epcArr = DataConversion.StrToHexByteArr(epc);
+            int writeFlag = StaticClassReaderB.WriteEPC_G2(ref comAdr, new byte[] { 0, 0, 0, 0 }, epcArr, (byte)epcArr.Length, ref errorcode, handle);
+
+            if (writeFlag != 0) throw UHF288Exception.AbnormalJudgment(writeFlag);
+        }
+    }
+}
---
+++ Model/ExceptionModels/UHF09Exception.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeviceService.Model.ExceptionModels
+{
+    public class UHF09Exception : Exception
+    {
+        public UHF09Exception(string message) : base(message)
+        {
+
+        }
+
+        /// <summary>
+        /// 返回UHF288设备异常信息
+        /// </summary>
+        /// <param name="state">异常状态</param>
+        /// <returns></returns>
+        public static UHF09Exception AbnormalJudgment(int state)
+        {
+            switch (state)
+            {
+                case 48:
+                    return new UHF09Exception("通讯错误。");
+                case 248:
+                    return new UHF09Exception("天线检测错误。");
+                default:
+                    return new UHF09Exception($"错误码：{state}");
+            }
+        }
+    }
+}
