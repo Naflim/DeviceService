4bb0ed03c9609d35a7e8e9757c01912ccdba7bfb
Naflim <naflimdante@gmail.com>
2022-04-08 15:04:55 +0800

    new

---
+++ DeviceModel/Arcface.cs
@@ -0,0 +1,35 @@
+﻿using ArcFaceSDK;
+using DeviceService.Model.ExceptionModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeviceService.DeviceModel
+{
+    public class Arcface
+    {
+        /// <summary>
+        /// 图像处理引擎对象
+        /// </summary>
+        protected FaceEngine imageEngine = new();
+
+        /// <summary>
+        /// 在线激活
+        /// </summary>
+        public void OnlineActivation(string appId,string sdkKey,string activeKey)
+        {
+            int activeFlag = imageEngine.ASFOnlineActivation(appId, sdkKey, activeKey);
+            if (activeFlag != 0 && activeFlag != 90114)
+                throw ArcfaceException.AbnormalJudgment(activeFlag);
+        }
+
+        public void OfflineActivation(string offlineActiveFilePath)
+        {
+            int activeFlag = imageEngine.ASFOfflineActivation(offlineActiveFilePath);
+            if (activeFlag != 0 && activeFlag != 90114)
+                throw ArcfaceException.AbnormalJudgment(activeFlag);
+        }
+    }
+}
---
+++ Model/ExceptionModels/ArcfaceException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace DeviceService.Model.ExceptionModels
+{
+    public class ArcfaceException : Exception
+    {
+        public ArcfaceException(string message) : base(message)
+        {
+
+        }
+
+        public static ArcfaceException AbnormalJudgment(int state)
+        {
+            return state switch
+            {
+                _ => new ArcfaceException($"错误码：{state}"),
+            };
+        }
+    }
+}
